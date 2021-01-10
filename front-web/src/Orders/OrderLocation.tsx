0c7714084592b728817317412e727f65e58af846
andersonsdsdev <adymaster.go@gmail.com>
2021-01-09 21:40:22 -0300

    location

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import AsyncSelect from 'react-select/async'
-import { fetchLocalMapBox } from './api'
-import { OrderLocationData } from './types'
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import AsyncSelect from 'react-select/async';
+import { fetchLocalMapBox } from './api';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
-    lat:  40.4319118,
-    lng: 116.5681862 
+    lat: -23.5976151,
+    lng: -46.6196707
 }
 
-type Place ={
+type Place = {
     label?: string;
     value?: string;
     position: {
@@ -18,72 +18,75 @@ type Place ={
     }
 }
 
-type Props ={
-    onChangeLocation: (location: OrderLocationData) => void
+type Props = {
+    onChangeLocation: (location: OrderLocationData) => void;
 }
 
 function OrderLocation({onChangeLocation}: Props) {
+
     const [address, setAddress] = useState<Place>({
         position: initialPosition
-    })
+    });
 
-    const loadOptions = async (inputValue: string, callback: (places: Place[])=> void ) =>{
+    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
         const response = await fetchLocalMapBox(inputValue);
-        const places = response.data.features.map((item:any)=>{
-            return({
-                label: item.place_name,
-                value: item.place_name,
-                position:{
-                    lat: item.center[1],
-                    lng: item.center[0]
-                },
-                place: item.place_name
-            })
-        })
-        callback(places)
-    };
-    
-    const handleChangeSelect = (place: Place) =>{
+      
+        const places = response.data.features.map((item: any) => {
+          return ({
+            label: item.place_name,
+            value: item.place_name,
+            position: {
+              lat: item.center[1],
+              lng: item.center[0]
+            }
+          });
+        });
+      
+        callback(places);
+      };
+      
+      const handleChangeSelect = (place: Place) => {
         setAddress(place);
         onChangeLocation({
-            latitude: place.position.lat,
-            longitude: place.position.lng,
-            address: place.label!
-        })
-    }
+          latitude: place.position.lat,
+          longitude: place.position.lng,
+          address: place.label!
+        }); 
+      };
 
     return (
         <div className="order-location-container">
-            <div className="order-location-content ">
+            <div className="order-location-content">
                 <h3 className="order-location-title">
                     Selecione onde o pedido deve ser entregue:
                 </h3>
-                <div className="filter-cntainer">
+                <div className="filter-container">
                     <AsyncSelect
-                    placeholder= "Digite um endereço para entregar o pedido"
-                    className="filter"
-                    loadOptions={loadOptions}
-                    onChange={value => handleChangeSelect(value as Place)}
+                        placeholder="Digite o endereço para a entrega"
+                        className="filter"
+                        loadOptions={loadOptions}
+                        onChange={value => handleChangeSelect(value as Place)}
                     />
                 </div>
-                <MapContainer center={address.position} zoom={15} key={address.position.lat}
-                    scrollWheelZoom={true}>
+                <MapContainer 
+                    center={address.position} 
+                    zoom={15} 
+                    key={address.position.lat}
+                    scrollWheelZoom
+                >
                     <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     <Marker position={address.position}>
-                    <Popup>
-                        {address.label}
-                    </Popup>
+                        <Popup>
+                            {address.label} 
+                        </Popup>
                     </Marker>
                 </MapContainer>
-
             </div>
         </div>
-        
     )
-        
-    
 }
+
 export default OrderLocation;
